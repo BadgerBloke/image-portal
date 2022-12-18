2704aeaa5509284729a903737b16f45ded0b91a2
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2022-12-18 19:19:06 +0530

    Allowed Host Updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-obxxrnunr&k$vg!p38)72r35(k2otml35cy+f$45-e3&kka(c9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # This should be changed in production
 CORS_ORIGIN_WHITELIST = [
